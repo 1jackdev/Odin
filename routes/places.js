f7d1151cc4e9736cb3afd686962fc319f214b42f
Jack <jackmayer21@gmail.com>
2021-07-28 00:39:07 -0700

    packages

--- routes/places.js
+++ routes/places.js
@@ -12,6 +12,7 @@ router.get("/:id", async function (req, res, next) {
     let resp = await axios.get(`https://api.yelp.com/v3/businesses/${id}`, {
       headers: {
         Authorization: "Bearer " + YELP_API_KEY,
+        AccessControlAllowOrigin: "*",
       },
     });
     let results = resp.data;
@@ -22,24 +23,3 @@ router.get("/:id", async function (req, res, next) {
 });
 
 module.exports = router;
-
-// async function getDetails({ place, location }) {
-//   let api_url = `https://api.yelp.com/v3/businesses/${place}`;
-//   // miles-placeholder / conversionFactor * 1000
-//   let resp = await axios.get(api_url, {
-//     headers: {
-//       Authorization: "Bearer " + YELP_API_KEY,
-//     },
-//   });
-//   let results = resp.data;
-//   let address = results.location.display_address;
-//   let googleResp = await axios.get(
-//     `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${location}&destinations=${address}&key=${GOOGLE_API_KEY}`
-//   );
-//   let distanceInMiles =
-//     googleResp.data.rows[0].elements[0].distance.value * 0.00062137;
-//     results.distanceInMiles = distanceInMiles.toFixed(2);
-//     return results;
-// }
-
-// export default getDetails;
