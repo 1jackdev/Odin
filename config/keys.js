b48dac02187504a338164b27ed6c2efea72104f2
Jack <jackmayer21@gmail.com>
2021-07-29 07:48:34 -0700

    fixing exports

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-if (process.env.NODE_ENV === "production") {
-  // we are in production - return the prod set of keys
-  export default require("./prod");
-} else {
-  // we are in development - return the dev keys!!
-  export default require("./dev");
-}
+if (process.env.NODE_ENV === 'production') {
+    // we are in production - return the prod set of keys
+    module.exports = require('./prod');
+  } else {
+    // we are in development - return the dev keys!!
+    module.exports = require('./dev');
+  }
--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
-export default {
+module.exports = {
   YELP_API_KEY: process.env.YELP_API_KEY,
 };
