3e0533a3f79461a0e28d5a178d2647dc64474b94
Jack <jackmayer21@gmail.com>
2022-06-20 18:34:34 -0500

    tweak config

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
-// Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   console.log(process.env.NODE_ENV);
   return process.env.NODE_ENV === "test"
